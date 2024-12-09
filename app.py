d894c45aba098448c2d1ed7c51af8152a8eacfa5
Tre21tyu <lonnieprogramming@gmail.com>
2024-12-09 00:36:41 -0500

    updated app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template
+from waitress import serve
+import webbrowser
+import threading
 
 app = Flask(__name__)
 
@@ -6,5 +9,12 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')  # Serves your HTML file
 
+def open_browser():
+    webbrowser.open("http://127.0.0.1:5000", new=2)  # Open the URL in a new window/tab
+
 if __name__ == '__main__':
-    app.run(port=5000, debug=False)
+    # Open the web browser window once when the application starts
+    threading.Thread(target=open_browser).start()
+
+    # Serve the app using Waitress
+    serve(app, host='127.0.0.1', port=5000)
